328f6bc7cf7bba7a57fbce5ce663b3a256046ee5
istale <iscarryon@gmail.com>
2015-03-13 00:34:59 +0800

    kevin: initial commit

---
+++ SubdomainRouteToArea/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SubdomainRouteToArea.Startup))]
+namespace SubdomainRouteToArea
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
